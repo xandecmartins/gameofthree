4a0cc5a2baf52a3c2b9ef86b3729ee3923fc33de
alexandre.cmartins <alexandre.cmartins@dataprev.gov.br>
2017-10-02 17:01:41 -0300

    warning remove

--- server/src/main/java/com/takeaway/gameofthree/server/controller/ServerController.java
+++ server/src/main/java/com/takeaway/gameofthree/server/controller/ServerController.java
@@ -7,17 +7,14 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import com.takeaway.gameofthree.domain.Player;
 import com.takeaway.gameofthree.domain.Status;
