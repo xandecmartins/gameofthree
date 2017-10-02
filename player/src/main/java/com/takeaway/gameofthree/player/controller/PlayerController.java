6a3d0224e73721e60db069920ab36f48c2551da2
alexandre.cmartins <alexandre.cmartins@dataprev.gov.br>
2017-10-02 19:42:56 -0300

    warning remove

--- player/src/main/java/com/takeaway/gameofthree/player/controller/PlayerController.java
+++ player/src/main/java/com/takeaway/gameofthree/player/controller/PlayerController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import com.takeaway.gameofthree.domain.CustomErrorType;
